edac1b46935deee4c233ed9867275dd59d83746e
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-07-07 09:48:35 +0200

    Affichage des routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', function () {
+    return "Tout les produits";
+});
+
+Route::get('/product/{id}', function ($id) {
+    return 'Fiche du produit ' .  $id;
+});
+
+Route::get('/cart', function () {
+    return 'Le panier';
+});
