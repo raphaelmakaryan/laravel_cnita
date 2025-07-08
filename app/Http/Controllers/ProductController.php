91013f64fbeec33fc2aaa93329e1770ddb0ae599
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-07-08 13:52:22 +0200

    Remplacement des requêtes DB par des méthodes Eloquent dans ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function explore()
     {
-        $allProduct = DB::select('select * from produits');
+        $allProduct = Product::all();
 
         return view("explore", data: ['produits' => $allProduct]);
     }
 
     public function detail($id)
     {
-        $productDetail = DB::select('select * from produits WHERE ID = ' . $id . '');
+        $productDetail = Product::where("ID", "=", $id)->get();
 
         return view("product-details", ["product"=> $productDetail]);
     }
